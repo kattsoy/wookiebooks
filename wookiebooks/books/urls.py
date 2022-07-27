b4bfa013ab8cacecc4cf5e17fcf6dcb9799fc7ac
«kattsoy» <kathkondratenko@gmail.com>
2022-07-27 15:23:26 +0200

    Books List/Detail API

--- wookiebooks/books/urls.py
+++ wookiebooks/books/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, BookAPIList, BookApiRetrive
 
 urlpatterns = [
     path('', index, name='home'),
+    path('api/v1/books/', BookAPIList.as_view()),
+    path('api/v1/books/<int:pk>/', BookApiRetrive.as_view()),
 ]
--- wookiebooks/books/views.py
+++ wookiebooks/books/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import Book
+from rest_framework import generics
+from .serializers import BookSerializer
 
 # Create your views here.
 def index(request):
     return render(request, 'books/index.html', {})
+
+class BookAPIList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookApiRetrive(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
